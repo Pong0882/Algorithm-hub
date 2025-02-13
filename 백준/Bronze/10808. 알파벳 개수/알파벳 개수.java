817909eb8253fc61b6f074812138d1d727267831
Pong0882 <77256939+Pong0882@users.noreply.github.com>
2025-02-13 17:39:43 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 68 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -0,0 +1,24 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int[] arr = new int[26];
+        char[] c = br.readLine().toCharArray();
+        for (int i = 0; i < c.length; i++) {
+            // int tmp = c[i] - 'a';
+            arr[c[i] - 'a']++;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(arr[i]).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
