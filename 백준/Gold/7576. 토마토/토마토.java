4c98ce296829eb24aaf1702f3bf96517aaf1ccee
HelloPong <77256939+Pong0882@users.noreply.github.com>
2025-03-02 13:33:58 +0900

    [Gold V] Title: 토마토, Time: 528 ms, Memory: 94716 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,60 +1,88 @@
-import java.io.*;
-import java.util.*;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
 
 public class Main {
-    private static int[][] data;
-    private static int N, M;
-    private static Queue<int[]> queue = new LinkedList<>();
-    private static int dirX[] = { 1, 0, -1, 0 };
-    private static int dirY[] = { 0, -1, 0, 1 };
+    // 이거 비지티드 하나로도 쓸수 있겠는데?
+
+    static boolean[][] visited;
+
+    static ArrayDeque<Integer> q1 = new ArrayDeque<>();
+    static ArrayDeque<Integer> q2 = new ArrayDeque<>();
+    static int[] dr = { 1, -1, 0, 0 };
+    static int[] dc = { 0, 0, 1, -1 };
+    static int cnt = 0;
+    static int result = -1;
+    static int R, C;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        C = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
+        visited = new boolean[R][C];
 
-        data = new int[N][M];
-
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < R; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
-                data[i][j] = Integer.parseInt(st.nextToken());
-                if (data[i][j] == 1) {
-                    queue.add(new int[] { i, j });
-                }
-
-            }
-        }
-
-        int result = 0;
-        while (!queue.isEmpty()) {
-            int[] now = queue.poll();
-            int x = now[0];
-            int y = now[1];
-
-            for (int i = 0; i < 4; i++) {
-                int tmpX = x + dirX[i];
-                int tmpY = y + dirY[i];
-
-                if (tmpX >= 0 && tmpY >= 0 && tmpX < N && tmpY < M && data[tmpX][tmpY] == 0) {
-                    data[tmpX][tmpY] = data[x][y] + 1;
-                    queue.add(new int[] { tmpX, tmpY });
-                }
-            }
-            result = data[x][y];
-        }
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (data[i][j] == 0) {
-                    result = 0;
+            for (int j = 0; j < C; j++) {
+                int tmp = Integer.parseInt(st.nextToken());
+                if (tmp == 0) {
+                    cnt++; // cnt 가 0이 아니면 -1 출력할것
+                } else if (tmp == -1) {
+                    visited[i][j] = true;
+                } else {
+                    q1.offer(i * C + j);
+                    visited[i][j] = true;
                 }
             }
         }
+        // for (int i = 0; i < R; i++) {
+        // System.out.println(Arrays.toString(visited[i]));
+        // }
+        while (!q1.isEmpty() || !q2.isEmpty()) {
+            result++;
+            if (!q1.isEmpty()) {
+                while (!q1.isEmpty()) {
+                    int tmp = q1.poll();
+                    int r = tmp / C;
+                    int c = tmp % C;
+                    for (int i = 0; i < 4; i++) {
+                        int nr = r + dr[i];
+                        int nc = c + dc[i];
+                        if (!check(nr, nc) || visited[nr][nc])
+                            continue;
+                        q2.offer(nr * C + nc);
+                        visited[nr][nc] = true;
+                        cnt--;
+                    }
+                }
+            } else {
+                while (!q2.isEmpty()) {
+                    int tmp = q2.poll();
+                    int r = tmp / C;
+                    int c = tmp % C;
+                    for (int i = 0; i < 4; i++) {
+                        int nr = r + dr[i];
+                        int nc = c + dc[i];
+                        if (!check(nr, nc) || visited[nr][nc])
+                            continue;
+                        q1.offer(nr * C + nc);
+                        visited[nr][nc] = true;
+                        cnt--;
+                    }
+                }
+            }
+        }
 
-        System.out.println(result - 1);
-    };
+        System.out.println(cnt == 0 ? result : -1);
+    }
+
+    private static boolean check(int nr, int nc) {
+        return nr >= 0 && nc >= 0 && nr < R && nc < C;
+    }
 }
