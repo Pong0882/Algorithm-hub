49a0ec822526cfa36968604442f72202993a9bb2
HelloPong <77256939+Pong0882@users.noreply.github.com>
2025-04-28 20:52:46 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 96 ms, Memory: 12828 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.nextInt(); // 입력된 n 값은 실제 계산에 사용되지 않음
+        System.out.println(1); // 코드1의 수행 횟수
+        System.out.println(0); // 최고차항의 차수 (상수 시간 → 0)
+    }
+}
