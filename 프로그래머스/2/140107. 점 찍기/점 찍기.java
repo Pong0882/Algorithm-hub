33b1ecaef337cc0f59ced1110c49709421333621
HelloPong <77256939+Pong0882@users.noreply.github.com>
2025-07-06 04:16:14 +0900

    [level 2] Title: 점 찍기, Time: 15.38 ms, Memory: 92.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -0,0 +1,23 @@
+class Solution {
+    public static long solution(int k, int d) {
+        long answer = 0;
+        for(int i=0; i<=d; i+=k){
+            int yMaxDistance = yPossibleDistance(i, d);
+            answer += yPossibleCount(yMaxDistance, k);
+        }
+        return answer;
+    }
+
+    private static int yPossibleDistance(int x, int d){
+        long xx = (long) Math.pow(x, 2);
+        long dd = (long) Math.pow(d, 2);
+
+        int result = (int)(Math.sqrt(dd-xx));
+        return result;
+    }
+
+    private static int yPossibleCount(int possible, int k){
+        int y = (possible/k);
+        return y+1;
+    }
+}
